33f13c7a9dcb75a32a5c859fda04dfbc097e5a3e
abrahamjo25 <abirham.johannes@gmail.com>
2024-06-26 14:23:42 +0300

    Adding search engine ref

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="canonical" href="https://www.atlaschania.gr/" />
       <body className={inter.className}>{children}</body>
     </html>
   );
